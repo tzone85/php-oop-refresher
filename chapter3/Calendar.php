8e61e4f526272537ed19264556e9efd4237d656f
Thando Mini <tzone85@gmail.com>
2023-03-11 06:12:41 +0200

    added a some type hinting to my properties and methods

--- chapter3/Calendar.php
+++ chapter3/Calendar.php
@@ -2,23 +2,24 @@
 
 namespace chapter3;
 define('YEAR', 2023);
+define('MONTHS_IN_YEAR', 12);
 class Calendar
 {
-    public $name;
-    public $seasons = ['Summer', 'Winter', 'Autumn', 'Spring'];
-    const MONTHS_IN_YEAR = 12;
-    static $days_in_february = 28;
+    public string $name;
+    public array $seasons = ['Summer', 'Winter', 'Autumn', 'Spring'];
+    public int $months_in_year= MONTHS_IN_YEAR;
+    static int $days_in_february = 28;
     static $count = 0;
-    public $weeks_in_a_year = 365 / 7;
-    public $year = YEAR;
+    public ?float $weeks_in_a_year = 365 / 7;
+    public ?int $year = YEAR;
 
     public function __construct()
     {
         self::$count++;
     }
-    public function getMonthsInYear()
+    public function getMonthsInYear(): ?int
     {
-        return self::MONTHS_IN_YEAR;
+        return $this->months_in_year;
     }
 }
 
@@ -28,13 +29,14 @@ $calendar2 = new Calendar();
 
 $calendar->name = 'Year Planner';
 print $calendar->name.PHP_EOL;
-print Calendar::MONTHS_IN_YEAR.PHP_EOL;
+print $calendar->getMonthsInYear().PHP_EOL;
+//print Calendar::MONTHS_IN_YEAR.PHP_EOL;
 Calendar::$days_in_february++;
 print $calendar::$days_in_february.PHP_EOL;
 //print Calendar::$days_in_february.PHP_EOL;
 
 print Calendar::$count.PHP_EOL;
 
-print_r($calendar->seasons).PHP_EOL;
+print_r($calendar->seasons, true).PHP_EOL;
 print $calendar->weeks_in_a_year.PHP_EOL;
 print $calendar->year.PHP_EOL;
